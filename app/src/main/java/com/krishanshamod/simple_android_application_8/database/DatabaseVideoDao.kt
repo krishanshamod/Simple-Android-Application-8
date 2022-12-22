6d95936b987bf8367fb7535dec3abc8b01c7488e
Krishan Shamod <krishanshamod@gmail.com>
2022-12-22 12:48:16 +0530

    Updated the DatabaseVideo Dao

--- app/src/main/java/com/krishanshamod/simple_android_application_8/database/DatabaseVideoDao.kt
+++ app/src/main/java/com/krishanshamod/simple_android_application_8/database/DatabaseVideoDao.kt
@@ -1,11 +1,13 @@
 package com.krishanshamod.simple_android_application_8.database
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.krishanshamod.simple_android_application_8.model.DatabaseVideo
 
+@Dao
 interface DatabaseVideoDao {
 
     @Query("SELECT * FROM DatabaseVideo")
