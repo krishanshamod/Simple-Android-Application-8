ec4a036605ae28af6a78678dfc6e57c848415167
Krishan Shamod <krishanshamod@gmail.com>
2022-12-22 09:38:34 +0530

    Added DevbyteVideo model class

---
+++ app/src/main/java/com/krishanshamod/simple_android_application_8/model/DevbyteVideo.kt
@@ -0,0 +1,9 @@
+package com.krishanshamod.simple_android_application_8.model
+
+data class DevbyteVideo(
+    val title: String,
+    val description: String,
+    val url: String,
+    val updated: String,
+    val thumbnail: String
+)
