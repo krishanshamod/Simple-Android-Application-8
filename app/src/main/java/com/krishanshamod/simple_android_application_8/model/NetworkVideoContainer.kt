588a79daeeb5ad5f0b2d6a27e6b12645139be908
Krishan Shamod <krishanshamod@gmail.com>
2022-12-22 12:50:31 +0530

    Added the asDomainModel converter function

--- app/src/main/java/com/krishanshamod/simple_android_application_8/model/NetworkVideoContainer.kt
+++ app/src/main/java/com/krishanshamod/simple_android_application_8/model/NetworkVideoContainer.kt
@@ -2,4 +2,16 @@ package com.krishanshamod.simple_android_application_8.model
 
 data class NetworkVideoContainer(
     val videos: List<NetworkVideo>
-)
+){
+    fun asDomainModel(): List<DevbyteVideo> {
+        return videos.map {
+            DevbyteVideo(
+                title = it.title,
+                description = it.description,
+                url = it.url,
+                updated = it.updated,
+                thumbnail = it.thumbnail
+            )
+        }
+    }
+}
