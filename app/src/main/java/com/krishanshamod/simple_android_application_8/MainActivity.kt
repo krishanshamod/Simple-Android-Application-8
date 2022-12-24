36dd43ae8858048aa1f82a83708da6f70ae80a3c
Krishan Shamod <krishanshamod@gmail.com>
2022-12-24 10:08:57 +0530

    Updated the main activity

--- app/src/main/java/com/krishanshamod/simple_android_application_8/MainActivity.kt
+++ app/src/main/java/com/krishanshamod/simple_android_application_8/MainActivity.kt
@@ -3,7 +3,9 @@ package com.krishanshamod.simple_android_application_8
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.krishanshamod.simple_android_application_8.ui.main.MainFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
