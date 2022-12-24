3ed120a6098405f77db2c87b3f8cb5aee1e15c37
Krishan Shamod <krishanshamod@gmail.com>
2022-12-24 10:07:47 +0530

    Overrided the application class to introduce the Hilt

---
+++ app/src/main/java/com/krishanshamod/simple_android_application_8/MVVMApplication.kt
@@ -0,0 +1,7 @@
+package com.krishanshamod.simple_android_application_8
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MVVMApplication : Application() {}
