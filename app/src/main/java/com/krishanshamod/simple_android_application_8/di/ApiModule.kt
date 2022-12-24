23dfc7e870306d207bb52c5d50688128049216d1
Krishan Shamod <krishanshamod@gmail.com>
2022-12-25 03:12:39 +0530

    Configured ApiModule and dependency providers

---
+++ app/src/main/java/com/krishanshamod/simple_android_application_8/di/ApiModule.kt
@@ -0,0 +1,31 @@
+package com.krishanshamod.simple_android_application_8.di
+
+import com.krishanshamod.simple_android_application_8.api.DevbyteApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://android-kotlin-fun-mars-server.appspot.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDevbyteApiService(retrofit: Retrofit) : DevbyteApiService {
+        return retrofit.create(DevbyteApiService::class.java)
+    }
+
+}
